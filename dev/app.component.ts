8e1a15910d9dba0244fbcdf23ce40cbb19a6c14a
Justin Thielke <Justin.Thielke@charter.com>
2016-01-21 14:09:21 -0700

    initial commit

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -1,9 +1,20 @@
 import {Component} from 'angular2/core';
 
 @Component({
-    selector: 'app',
-    template: 'Hello World!',
+    selector: 'thing',
+    templateUrl: 'dev/app.html',
+    styleUrls: ["../src/css/app.css"]
 })
 export class AppComponent {
+    public contact = {firstName: "Justin", lastName: "Thielke", phone: "303 582 4807", email: "jethielke@gmail.com"};
+    public showDetail = false;
 
+    onSelect() {
+        if(this.showDetail){
+            this.showDetail = false;
+        }
+        else {
+            this.showDetail = true;
+        }
+    }
 }
